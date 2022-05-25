b4cea8c2a91dbc2a3a3f9365d6ba68f115ab9d57
Maximilian Wagner <maw3350@gmail.com>
2022-05-25 00:43:37 -0400

    feat: add login functionalily with sad path error handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,21 @@ class UsersController < ApplicationController
       redirect_to "/users/#{@user.id}"
     else
       flash.now[:user_errors] = 'User Not Registered'
-      render 'new'
+      render :new
+    end
+  end
+
+  def login_form
+
+  end
+
+  def login_user
+    @user = User.find_by(email: params[:email])
+    if @user.authenticate(params[:password])
+      redirect_to "/users/#{@user.id}", notice: 'Logged in successfully'
+    else
+      flash.now[:alert] = 'Invalid email or password'
+      render :login_form
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
 
-  get '/users/:id', to: 'users#show'
+  get '/login', to: 'users#login_form'
+  post '/login', to: 'users#login_user'
+
+  get '/users/:id', to: 'users#show', as: 'user_show'
   get '/users/:id/discover', to: 'discover#index', as: 'discover_index'
   get '/users/:id/movies', to: 'movies#index', as: 'movie_index'
   get '/users/:user_id/movies/:id', to: 'movies#show', as: 'movie_show'
