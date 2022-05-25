35b397fa2161be4143f6a9e0d0e6b1d1836d7c63
Maximilian Wagner <maw3350@gmail.com>
2022-05-24 20:16:56 -0400

    feat: update view/feature testing for creating user (require password/confirmation)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params)
-    redirect_to "/users/#{user.id}"
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to "/users/#{@user.id}"
+    else
+      redirect_to "/register"
+    end
   end
 
   private
 
   def user_params
-      params.permit(:name, :email)
+      params.permit(:name, :email, :password, :password_confirmation)
   end
 end
--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -3,22 +3,28 @@ require 'rails_helper'
 RSpec.describe "Registration Page", type: :feature do
   describe "when a visitor visits /register" do
     before (:each) do
-      @user1 = User.create!(name: "Sabin Figaro", email: "sabinff6@square.com")
-      @user2 = User.create!(name: "Edgar Figaro", email: "edgarff6@square.com")
+      # @user1 = User.create!(name: "Sabin Figaro", email: "sabinff6@square.com")
+      # @user2 = User.create!(name: "Edgar Figaro", email: "edgarff6@square.com")
       visit '/register'
     end
 
     it "has a form" do
+
       expect(page).to have_content("Name:")
       expect(page).to have_content("Email:")
+      expect(page).to have_content("Password:")
+      expect(page).to have_content("Password confirmation:")
     end
 
     it "creates a new user" do
-      expect(User.all).to eq([@user1, @user2])
+      # expect(User.all).to eq([@user1, @user2])
 
 
       fill_in("name", with: "Celeste Chere")
       fill_in("email", with: "celesff6@square.com")
+      fill_in("password", with: "abc123")
+      # save_and_open_page
+      fill_in("password_confirmation", with: "abc123")
       click_on "Register"
 
       user3 = User.last
@@ -31,11 +37,13 @@ RSpec.describe "Registration Page", type: :feature do
 
       fill_in("name", with: "Celeste Chere")
       fill_in("email", with: "celesff6@square.com")
+      fill_in("password", with: "abc123")
+      fill_in("password_confirmation", with: "abc123")
       click_on "Register"
 
-      user3 = User.last
+      @user3 = User.last
 
-      expect(current_path).to eq("/users/#{user3.id}")
+      expect(current_path).to eq("/users/#{@user3.id}")
     end
   end
 end
